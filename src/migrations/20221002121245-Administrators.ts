f24bd8590a2808c88010d800df6ca27bceac08c4
Demid <d_greshnikov@tlen.pl>
2022-10-02 14:58:27 +0200

    stworzenie tabeli administratora oraz dodanie administratora

---
+++ src/migrations/20221002121245-Administrators.ts
@@ -0,0 +1,36 @@
+import { QueryInterface } from 'sequelize';
+import { DataType } from 'sequelize-typescript';
+
+module.exports = {
+	async up(queryInterface: QueryInterface) {
+		queryInterface.createTable('administrators', {
+			id: {
+				allowNull: false,
+				primaryKey: true,
+				type: DataType.UUID,
+				defaultValue: DataType.UUIDV4
+			},
+			name: {
+				allowNull: false,
+				type: DataType.STRING
+			},
+			login: {
+				allowNull: false,
+				type: DataType.STRING
+			},
+			password: {
+				allowNull: false,
+				type: DataType.STRING
+			},
+			isAdmin: {
+				allowNull: true,
+				defaultValue: false,
+				type: DataType.BOOLEAN
+			}
+		});
+	},
+
+	async down(queryInterface: QueryInterface) {
+		queryInterface.dropTable('administrators');
+	}
+};
---
+++ src/migrations/20221002123253-InsertAdmin.ts
@@ -0,0 +1,23 @@
+import { QueryInterface } from 'sequelize';
+const bcrypt = require('bcrypt');
+const saltRounds = 13;
+
+module.exports = {
+	async up(queryInterface: QueryInterface) {
+		return queryInterface.bulkInsert('administrators', [
+			{
+				id: '04ca1979-f150-46b3-a217-956395ffe53c',
+				name: 'admin',
+				login: 'admin',
+				password: await bcrypt.hash('admin', saltRounds),
+				isAdmin: true
+			}
+		]);
+	},
+
+	async down(queryInterface: QueryInterface) {
+		return queryInterface.bulkDelete('administrators', {
+			name: 'admin'
+		});
+	}
+};
