d76e8fae72729371e2218b1034aa7e8264941eac
Demid <d_greshnikov@tlen.pl>
2022-11-05 12:59:42 +0100

    poprawka configu

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,11 @@
 // sequelize database connection configuration
 module.exports = {
   development: {
-    username: "root",
-    password: "admin",
-    database: "korty",
-    host: "127.0.0.1",
-    port: 3306,
+    username: process.env.DATABASE_username,
+    password: process.env.DATABASE_password,
+    database: process.env.DATABASE_database,
+    host: process.env.DATABASE_host,
+    port: process.env.DATABASE_port,
     dialect: "mysql",
     migrationStorageTableName: "migrations",
   },
