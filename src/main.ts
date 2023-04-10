31ad382990a20257a658803a80cab8b2b6d326ba
Demid <d_greshnikov@tlen.pl>
2023-04-10 17:44:00 +0200

    fix body validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
 import { AppModule } from "./app.module";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(helmet());
   if (process.env.MODE === "dev") {
     app.enableCors({
       origin: ["http://localhost:4200", "http://localhost:3000"],
