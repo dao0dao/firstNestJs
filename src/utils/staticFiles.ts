4224ee310255fdc6d7a220f383bcd96b5e83301c
Demid <d_greshnikov@tlen.pl>
2023-04-12 15:34:22 +0200

    move front package

--- src/utils/staticFiles.ts
+++ src/utils/staticFiles.ts
@@ -2,9 +2,9 @@ import { ServeStaticModuleOptions } from "@nestjs/serve-static";
 import { join, dirname } from "path";
 
 const appDir: string = dirname(require.main.filename);
-export const frontStaticFolder = join(appDir, "front");
+export const frontStaticFolder = join(appDir, "..", "front");
 export const frontIndexFile = join(frontStaticFolder, "index.html");
-export const logsFolderPath = join(appDir, "..", "..", "logs");
+export const logsFolderPath = join(appDir, "..", "logs");
 
 export const StaticModuleForRoot: ServeStaticModuleOptions = {
   rootPath: frontStaticFolder,
