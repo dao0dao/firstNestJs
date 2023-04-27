d4f5cd7c2a51548540d23a76962af0a54857d90e
Demid <d_greshnikov@tlen.pl>
2023-04-27 22:27:29 +0200

    replace node:fs/promises to fs

--- src/utils/writeLogs.ts
+++ src/utils/writeLogs.ts
@@ -1,37 +1,45 @@
-// import { access, mkdir, writeFile } from "node:fs/promises";
-// import { join } from "path";
+import { access, mkdir, writeFile } from "fs";
+import { join } from "path";
 import { ValidationError } from "class-validator";
-// import { logsFolderPath } from "src/utils/staticFiles";
+import { logsFolderPath } from "src/utils/staticFiles";
 
 function createErrorLog(errors: ValidationError[]) {
-  // let content = "";
-  // for (const e of errors) {
-  //   const obj = {
-  //     name: e.property,
-  //     constraints: e.constraints,
-  //     value: e.value,
-  //   };
-  //   content = content + e.toString() + JSON.stringify(obj) + "\n ----- \n";
-  // }
-  // const date = new Date();
-  // const fileName = date.toISOString().replace(/:/g, "-") + ".log";
-  // const pathToFile = join(logsFolderPath, fileName);
-  // writeFile(pathToFile, content, { encoding: "utf-8" });
+  let content = "";
+  for (const e of errors) {
+    const obj = {
+      name: e.property,
+      constraints: e.constraints,
+      value: e.value,
+    };
+    content = content + e.toString() + JSON.stringify(obj) + "\n ----- \n";
+  }
+  const date = new Date();
+  const fileName = date.toISOString().replace(/:/g, "-") + ".log";
+  const pathToFile = join(logsFolderPath, fileName);
+  writeFile(pathToFile, content, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
 
 export async function writeErrorToLog(errors: ValidationError[]) {
-  // const checkFolder = new Promise<boolean>((resolve) => {
-  //   access(logsFolderPath)
-  //     .then(() => {
-  //       resolve(true);
-  //     })
-  //     .catch(() => {
-  //       resolve(false);
-  //     });
-  // });
-  // const isFolderExist = await checkFolder;
-  // if (!isFolderExist) {
-  //   await mkdir(logsFolderPath);
-  // }
-  // createErrorLog(errors);
+  const checkFolder = new Promise<boolean>((resolve) => {
+    access(logsFolderPath, (err) => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+  const isFolderExist = await checkFolder;
+  if (!isFolderExist) {
+    mkdir(logsFolderPath, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+  createErrorLog(errors);
 }
