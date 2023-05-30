ff1b58679f2badc5b71a283bf5d853058f0832a6
Demid <d_greshnikov@tlen.pl>
2023-05-30 22:35:20 +0200

    add test

--- src/guards/auth.guard.spec.ts
+++ src/guards/auth.guard.spec.ts
@@ -1,5 +1,5 @@
 import { AuthGuard } from "./auth.guard";
-import { ExecutionContext } from "@nestjs/common";
+import { ExecutionContext, UnauthorizedException } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserSQLService } from "src/models/model/user/user-sql.service";
@@ -7,16 +7,16 @@ import { SessionsService } from "src/utils/shared/session.service";
 
 describe("AuthGuard", () => {
   let authGuard: AuthGuard;
-  const reflector = {
+  const mockReflector = {
     get: jest.fn(),
   };
-  const sessionService = {
+  const mockSessionService = {
     findLoginUserBySessionId: jest.fn(),
   };
   const adminService = {
     findUserById: jest.fn(),
   };
-  const context = {
+  const mockContext = {
     switchToHttp: jest.fn(() => ({
       getRequest: jest.fn(() => ({
         cookies: { key: "session_id" },
@@ -24,13 +24,13 @@ describe("AuthGuard", () => {
       getResponse: jest.fn(),
     })),
     getHandler: jest.fn(),
-  };
+  } as unknown as ExecutionContext;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        { provide: Reflector, useValue: reflector },
-        { provide: SessionsService, useValue: sessionService },
+        { provide: Reflector, useValue: mockReflector },
+        { provide: SessionsService, useValue: mockSessionService },
         { provide: UserSQLService, useValue: adminService },
         AuthGuard,
       ],
@@ -38,116 +38,123 @@ describe("AuthGuard", () => {
     authGuard = module.get<AuthGuard>(AuthGuard);
   });
 
-  describe("canActivate", () => {
-    it("should be define", () => {
-      expect(authGuard).toBeDefined();
-    });
-    // it("should return true if request role is not specified", async () => {
-    //   reflector.get.mockReturnValueOnce(undefined);
-    //   const result = await authGuard.canActivate(context);
-    //   expect(result).toBe(true);
-    // });
-    // it("should throw UnauthorizedException if session_id is not provided", async () => {
-    //   context.switchToHttp().getRequest.mockReturnValueOnce({ cookies: {} });
-    //   await expect(authGuard.canActivate(context)).rejects.toThrow(
-    //     UnauthorizedException
-    //   );
-    // });
-    // it("should throw UnauthorizedException if loginUser is not found", async () => {
-    //   sessionService.findLoginUserBySessionId.mockReturnValueOnce(null);
-    //   const clearCookieMock = jest.fn();
-    //   const getResponseMock = jest.fn(() => ({
-    //     clearCookie: clearCookieMock,
-    //   }));
-    //   context.switchToHttp().getResponse.mockReturnValueOnce(getResponseMock);
-    //   await expect(authGuard.canActivate(context)).rejects.toThrow(
-    //     HttpException
-    //   );
-    //   expect(clearCookieMock).toHaveBeenCalledWith("key");
-    //   expect(getResponseMock).toHaveBeenCalledWith();
-    //   expect(getResponseMock().clearCookie).toHaveBeenCalledWith("key");
-    //   expect(HttpException).toHaveBeenCalledWith(
-    //     { session: "fail" },
-    //     HttpStatus.UNAUTHORIZED
-    //   );
-    // });
-    // it("should throw UnauthorizedException if registeredUser is not found", async () => {
-    //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
-    //     administrator_id: "admin_id",
-    //   });
-    //   adminService.findUserById.mockReturnValueOnce(null);
-    //   await expect(authGuard.canActivate(context)).rejects.toThrow(
-    //     UnauthorizedException
-    //   );
-    // });
-    // it("should set ADMIN_NAME and ADMIN_ID in the request if registeredUser is found and isAdmin is true", async () => {
-    //   const registeredUser = {
-    //     name: "John",
-    //     id: "user_id",
-    //     isAdmin: true,
-    //   };
-    //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
-    //     administrator_id: "admin_id",
-    //   });
-    //   adminService.findUserById.mockReturnValueOnce(registeredUser);
-    //   reflector.get.mockReturnValueOnce("admin");
-    //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
-    //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
-    //   await authGuard.canActivate(context);
-    //   expect(requestDTO.ADMIN_NAME).toBe(registeredUser.name);
-    //   expect(requestDTO.ADMIN_ID).toBe(registeredUser.id);
-    //   expect(requestDTO.ROLE).toBe("admin");
-    // });
-    // it("should set ADMIN_NAME and ADMIN_ID in the request if registeredUser is found and isAdmin is false", async () => {
-    //   const registeredUser = {
-    //     name: "John",
-    //     id: "user_id",
-    //     isAdmin: false,
-    //   };
-    //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
-    //     administrator_id: "admin_id",
-    //   });
-    //   adminService.findUserById.mockReturnValueOnce(registeredUser);
-    //   reflector.get.mockReturnValueOnce("login");
-    //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
-    //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
-    //   await authGuard.canActivate(context);
-    //   expect(requestDTO.ADMIN_NAME).toBe(registeredUser.name);
-    //   expect(requestDTO.ADMIN_ID).toBe(registeredUser.id);
-    //   expect(requestDTO.ROLE).toBe("login");
-    // });
-    // it("should return true if requestRole is included in roles array", async () => {
-    //   const registeredUser = {
-    //     name: "John",
-    //     id: "user_id",
-    //     isAdmin: true,
-    //   };
-    //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
-    //     administrator_id: "admin_id",
-    //   });
-    //   adminService.findUserById.mockReturnValueOnce(registeredUser);
-    //   reflector.get.mockReturnValueOnce("admin");
-    //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
-    //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
-    //   const result = await authGuard.canActivate(context);
-    //   expect(result).toBe(true);
-    // });
-    // it("should throw UnauthorizedException if requestRole is not included in roles array", async () => {
-    //   const registeredUser = {
-    //     name: "John",
-    //     id: "user_id",
-    //     isAdmin: true,
-    //   };
-    //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
-    //     administrator_id: "admin_id",
-    //   });
-    //   adminService.findUserById.mockReturnValueOnce(registeredUser);
-    //   reflector.get.mockReturnValueOnce("guest");
-    //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
-    //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
-    //   await expect(authGuard.canActivate(context)).rejects.toThrow(
-    //     UnauthorizedException
-    //   );
-    // });
+  it("should be define", () => {
+    expect(authGuard).toBeDefined();
   });
+  it("should return true if request role is not specified", async () => {
+    mockReflector.get.mockReturnValueOnce(undefined);
+    const result = await authGuard.canActivate(mockContext);
+    expect(result).toBe(true);
+  });
+  it("should throw UnauthorizedException if session_id is not provided", async () => {
+    const newMockContext = {
+      switchToHttp: jest.fn(() => ({
+        getRequest: jest.fn(() => ({
+          cookies: {},
+        })),
+        getResponse: jest.fn(),
+      })),
+      getHandler: jest.fn(),
+    } as unknown as ExecutionContext;
+    mockReflector.get.mockReturnValueOnce("login");
+    await expect(authGuard.canActivate(newMockContext)).rejects.toThrow(
+      UnauthorizedException
+    );
+  });
+  // it("should throw UnauthorizedException if loginUser is not found", async () => {
+  //   sessionService.findLoginUserBySessionId.mockReturnValueOnce(null);
+  //   const clearCookieMock = jest.fn();
+  //   const getResponseMock = jest.fn(() => ({
+  //     clearCookie: clearCookieMock,
+  //   }));
+  //   context.switchToHttp().getResponse.mockReturnValueOnce(getResponseMock);
+  //   await expect(authGuard.canActivate(context)).rejects.toThrow(
+  //     HttpException
+  //   );
+  //   expect(clearCookieMock).toHaveBeenCalledWith("key");
+  //   expect(getResponseMock).toHaveBeenCalledWith();
+  //   expect(getResponseMock().clearCookie).toHaveBeenCalledWith("key");
+  //   expect(HttpException).toHaveBeenCalledWith(
+  //     { session: "fail" },
+  //     HttpStatus.UNAUTHORIZED
+  //   );
+  // });
+  // it("should throw UnauthorizedException if registeredUser is not found", async () => {
+  //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
+  //     administrator_id: "admin_id",
+  //   });
+  //   adminService.findUserById.mockReturnValueOnce(null);
+  //   await expect(authGuard.canActivate(context)).rejects.toThrow(
+  //     UnauthorizedException
+  //   );
+  // });
+  // it("should set ADMIN_NAME and ADMIN_ID in the request if registeredUser is found and isAdmin is true", async () => {
+  //   const registeredUser = {
+  //     name: "John",
+  //     id: "user_id",
+  //     isAdmin: true,
+  //   };
+  //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
+  //     administrator_id: "admin_id",
+  //   });
+  //   adminService.findUserById.mockReturnValueOnce(registeredUser);
+  //   reflector.get.mockReturnValueOnce("admin");
+  //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
+  //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
+  //   await authGuard.canActivate(context);
+  //   expect(requestDTO.ADMIN_NAME).toBe(registeredUser.name);
+  //   expect(requestDTO.ADMIN_ID).toBe(registeredUser.id);
+  //   expect(requestDTO.ROLE).toBe("admin");
+  // });
+  // it("should set ADMIN_NAME and ADMIN_ID in the request if registeredUser is found and isAdmin is false", async () => {
+  //   const registeredUser = {
+  //     name: "John",
+  //     id: "user_id",
+  //     isAdmin: false,
+  //   };
+  //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
+  //     administrator_id: "admin_id",
+  //   });
+  //   adminService.findUserById.mockReturnValueOnce(registeredUser);
+  //   reflector.get.mockReturnValueOnce("login");
+  //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
+  //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
+  //   await authGuard.canActivate(context);
+  //   expect(requestDTO.ADMIN_NAME).toBe(registeredUser.name);
+  //   expect(requestDTO.ADMIN_ID).toBe(registeredUser.id);
+  //   expect(requestDTO.ROLE).toBe("login");
+  // });
+  // it("should return true if requestRole is included in roles array", async () => {
+  //   const registeredUser = {
+  //     name: "John",
+  //     id: "user_id",
+  //     isAdmin: true,
+  //   };
+  //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
+  //     administrator_id: "admin_id",
+  //   });
+  //   adminService.findUserById.mockReturnValueOnce(registeredUser);
+  //   reflector.get.mockReturnValueOnce("admin");
+  //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
+  //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
+  //   const result = await authGuard.canActivate(context);
+  //   expect(result).toBe(true);
+  // });
+  // it("should throw UnauthorizedException if requestRole is not included in roles array", async () => {
+  //   const registeredUser = {
+  //     name: "John",
+  //     id: "user_id",
+  //     isAdmin: true,
+  //   };
+  //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
+  //     administrator_id: "admin_id",
+  //   });
+  //   adminService.findUserById.mockReturnValueOnce(registeredUser);
+  //   reflector.get.mockReturnValueOnce("guest");
+  //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
+  //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
+  //   await expect(authGuard.canActivate(context)).rejects.toThrow(
+  //     UnauthorizedException
+  //   );
+  // });
 });
