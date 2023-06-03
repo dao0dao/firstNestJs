d4dab64c17787f3f22cd1c2b3060fa94e98cd8ba
Demid <d_greshnikov@tlen.pl>
2023-06-03 20:36:34 +0200

    finish auth guard unit test

--- src/guards/auth.guard.spec.ts
+++ src/guards/auth.guard.spec.ts
@@ -97,7 +97,7 @@ describe("AuthGuard", () => {
       HttpException
     );
   });
-  it("should set ADMIN_NAME and ADMIN_ID in the request if registeredUser is found and isAdmin is true", async () => {
+  it("should return true if registeredUser is found and isAdmin is false", async () => {
     const mockUser = true as unknown as Promise<any>;
     jest
       .spyOn(sessionsService, "findLoginUserBySessionId")
@@ -111,55 +111,20 @@ describe("AuthGuard", () => {
     const result = await authGuard.canActivate(mockContext);
     expect(result).toEqual(true);
   });
-  // it("should set ADMIN_NAME and ADMIN_ID in the request if registeredUser is found and isAdmin is false", async () => {
-  //   const registeredUser = {
-  //     name: "John",
-  //     id: "user_id",
-  //     isAdmin: false,
-  //   };
-  //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
-  //     administrator_id: "admin_id",
-  //   });
-  //   adminService.findUserById.mockReturnValueOnce(registeredUser);
-  //   reflector.get.mockReturnValueOnce("login");
-  //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
-  //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
-  //   await authGuard.canActivate(context);
-  //   expect(requestDTO.ADMIN_NAME).toBe(registeredUser.name);
-  //   expect(requestDTO.ADMIN_ID).toBe(registeredUser.id);
-  //   expect(requestDTO.ROLE).toBe("login");
-  // });
-  // it("should return true if requestRole is included in roles array", async () => {
-  //   const registeredUser = {
-  //     name: "John",
-  //     id: "user_id",
-  //     isAdmin: true,
-  //   };
-  //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
-  //     administrator_id: "admin_id",
-  //   });
-  //   adminService.findUserById.mockReturnValueOnce(registeredUser);
-  //   reflector.get.mockReturnValueOnce("admin");
-  //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
-  //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
-  //   const result = await authGuard.canActivate(context);
-  //   expect(result).toBe(true);
-  // });
-  // it("should throw UnauthorizedException if requestRole is not included in roles array", async () => {
-  //   const registeredUser = {
-  //     name: "John",
-  //     id: "user_id",
-  //     isAdmin: true,
-  //   };
-  //   sessionService.findLoginUserBySessionId.mockReturnValueOnce({
-  //     administrator_id: "admin_id",
-  //   });
-  //   adminService.findUserById.mockReturnValueOnce(registeredUser);
-  //   reflector.get.mockReturnValueOnce("guest");
-  //   const requestDTO: RequestDTO = { cookies: { key: "session_id" } };
-  //   context.switchToHttp().getRequest.mockReturnValueOnce(requestDTO);
-  //   await expect(authGuard.canActivate(context)).rejects.toThrow(
-  //     UnauthorizedException
-  //   );
-  // });
+  it("should throw UnauthorizedException if registeredUser is found and isAdmin is false and role is admin", async () => {
+    const mockUser = true as unknown as Promise<any>;
+    jest
+      .spyOn(sessionsService, "findLoginUserBySessionId")
+      .mockReturnValue(mockUser);
+    mockReflector.get.mockReturnValueOnce("admin");
+    const registeredUser = {
+      name: "Jan",
+      id: "some_id",
+      isAdmin: false,
+    } as unknown as Promise<any>;
+    jest.spyOn(adminService, "findUserById").mockReturnValue(registeredUser);
+    await expect(authGuard.canActivate(mockContext)).rejects.toThrow(
+      UnauthorizedException
+    );
+  });
 });
