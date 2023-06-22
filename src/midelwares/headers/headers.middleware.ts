bbe20c2496e72a9d79f3c3b345f141e1f7106ff0
Demid <d_greshnikov@tlen.pl>
2023-06-22 21:31:58 +0200

    fix header

--- src/midelwares/headers/headers.middleware.ts
+++ src/midelwares/headers/headers.middleware.ts
@@ -6,10 +6,15 @@ export class HeadersMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     res
       .setHeader("Content-Security-Policy", [
-        "default-src 'self' 'unsafe-inline' 'https://fonts.gstatic.com'; image-src 'self'; object-src 'self'; font-src 'self' https://fonts.gstatic.com ; style-src 'self' 'unsafe-inline'; connect-src 'self'; referrer-policy 'no-referrer'",
-        "X-Frame-Options 'DENY'",
-        "Strict-Transport-Security 'max-age=31536000' 'includeSubDomains'",
-        "X-Content-Type-Options, 'nosniff'",
+        "default-src 'self' 'unsafe-inline'; object-src 'none'; font-src 'self' https://fonts.gstatic.com ; style-src 'self' 'unsafe-inline'; connect-src 'self'",
+      ])
+      .setHeader("referrer-policy", "no-referrer")
+      .setHeader("image-src", "self")
+      .setHeader("X-Frame-Options", "DENY")
+      .setHeader("X-Content-Type-Options", "nosniff")
+      .setHeader("Strict-Transport-Security", [
+        "max-age=31536000",
+        "includeSubDomains",
       ])
       .removeHeader("Keep-Alive");
     next();
