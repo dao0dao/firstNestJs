cb69e9970b9e063b07ffb48beb0c5d3bb30ef220
Demid <d_greshnikov@tlen.pl>
2023-06-22 18:25:23 +0200

    fix header

--- src/midelwares/headers/headers.middleware.ts
+++ src/midelwares/headers/headers.middleware.ts
@@ -6,7 +6,7 @@ export class HeadersMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     res
       .setHeader("Content-Security-Policy", [
-        "default-src 'self' 'unsafe-inline' 'https://fonts.gstatic.com'; script-src 'self' 'unsafe-inline'; image-src 'self'; object-src 'self'; font-src 'self' https://fonts.gstatic.com ; style-src 'self' 'unsafe-inline'; connect-src 'self'; referrer-policy 'no-referrer'",
+        "default-src 'self' 'unsafe-inline' 'https://fonts.gstatic.com'; image-src 'self'; object-src 'self'; font-src 'self' https://fonts.gstatic.com ; style-src 'self' 'unsafe-inline'; connect-src 'self'; referrer-policy 'no-referrer'",
         "X-Frame-Options 'DENY'",
         "Strict-Transport-Security 'max-age=31536000' 'includeSubDomains'",
         "X-Content-Type-Options, 'nosniff'",
